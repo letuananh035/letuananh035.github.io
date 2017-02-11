3b6c233dad581cd6824148757989571cd6546f5a
Lê Tuấn Anh <letuananh035@gmail.com>
2017-02-11 22:16:56 +0700

    Add files via upload

---
+++ hdonlinejs.js
@@ -0,0 +1,21 @@
+function loadXMLDoc(url) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+	  var res = this.responseURL.split("ip=");
+	  document.cookie = "token=" + res[1];
+	  alert(this.responseURL);
+      return this.responseURL;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+
+}
+
+function clickedButton()
+{
+	var text = $("#text-search").val(); 
+	window.location.assign('search.php?text=' + text);
+	return false;
+}
